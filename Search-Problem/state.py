3636913d7ca8a05909fead1d61af20d6d88f4e4f
Silvia452 <100429118@alumnos.uc3m.es>
2021-12-17 13:47:04 +0100

    State version

--- Search-Problem/state.py
+++ Search-Problem/state.py
@@ -3,7 +3,10 @@
                             State (Ports, Boat)
 """
 import re
+import sys
 
+CRED = '\033[91m'
+CEND = '\033[0m'
 
 class Boat(object):
     def __init__(self, port: int, stowage: list):
@@ -39,26 +42,32 @@ class State:
         return False
 
     def get_init_goal(self, file_containers, file_layout):
-        # TODO: obtain containers for port0 from file
-        file_containers.open()
-        port0 = []
 
+
+        #obtain containers for port0 from file
+
+        port0 = self.init_containers(file_containers)
         initPorts = [port0, [], []]
 
-        # TODO: obtain layout of Boat Stowage from file
-        file_layout.open()
-        layout0 = []
+        #obtain layout of Boat Stowage from file
+
+        layout0 = self.init_map(file_layout)
         initBoat = Boat(0, layout0)
 
         self.INITIAL_STATE = (initPorts, initBoat)
 
-        # TODO: obtain final distribution of ports
+        #obtain final distribution of ports
         port1 = []
         port2 = []
+        for c in port0:
+            if c[2] == 1:
+                port1.append(c)
+            elif c[2] == 2:
+                port2.append(c)
 
         finalPorts = [[], port1, port2]
 
-        # obtain final layout of Boat
+        #obtain final layout of Boat
         finalBoat = Boat(2, layout0)
 
         self.FINAL_STATE = (finalPorts, finalBoat)
@@ -68,3 +77,72 @@ class State:
 
     def get_Final(self):
         return self.FINAL_STATE
+
+    def init_map(self, file):
+            """From file map: (stack, depth)
+                        N N N N     (0,0) (1,0) (2,0) (3,0)
+                        N N N N     (0,1) (1,1) (1,2) (3,1)
+                        E N N E     (0,2) (1,2) (2,2) (3,2)
+                        X E E X           (1,3) (2,3)
+                        X X X X
+            """
+            layout = []
+            with open(file) as handle:
+                raw_lines = handle.read().split('\n')
+
+            for n in range(len(raw_lines[0].split())):
+                layout.append([])
+
+            for line in raw_lines:
+                regex = r"((N|E|X)\s)*(N|E|X){1}(\n)*"
+
+                line_list = line.split()  # => [N,N,N,N]
+
+                for n in range(len(line_list)):
+                    if re.match(regex, line_list[n]) is None:
+                        print(line_list[n])
+                        print(CRED + 'error parsing test file: %s' % file + CEND)
+                        sys.exit(1)
+                    else:
+                        layout[n].append(line_list[n])
+            """Read output:
+                    => layout =  [  ['N', 'N', 'E', 'X', 'X'], 
+                                    ['N', 'N', 'N', 'E', 'X'],
+                                    ['N', 'N', 'N', 'E', 'X'],
+                                    ['N', 'N', 'E', 'X', 'X']
+                                ]
+
+            """
+
+            return layout
+
+        def init_containers(self, file):
+            """
+            From file containers:
+                1 S 1
+                2 S 1
+                3 S 1
+                4 R 2
+                5 R 2
+
+            Read output:
+                => containers = [(1,S,1), (2,S,1), (3,S,1), (4,R,2), (5,R,2)]
+
+                containers[i] = "i"
+            """
+            containers = []
+
+            with open(file) as handle:
+                raw_lines = handle.read().split('\n')
+
+            for line in raw_lines:
+                regex = r"\d+ (S|R) (1|2)\n?"
+
+                if not re.match(regex, line):
+                    print(CRED + 'error parsing test file: %s' % file + CEND)
+                    sys.exit(1)
+                else:
+                    containers.append(tuple(line.split()))
+
+            return containers
+
